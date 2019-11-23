51ca44125b5920c75a18b09fe34dc5178d8d084e
Gamaranto <gamarantor@gmail.com>
2019-11-23 19:18:22 +0100

    bug fix

--- src/extractors/casait/casait.js
+++ src/extractors/casait/casait.js
@@ -2,7 +2,10 @@ const extractInfo = require("./extractInfo");
 const extractSchema = require("./extractSchema");
 
 module.exports = function(context) {
-  const { $ } = context;
+  const { $, request } = context;
+  if (request.url == "https://www.casa.it/affitto/residenziale/pisa") {
+    return {};
+  }
   const schema = extractSchema($);
   return extractInfo({ $, schema });
 };
